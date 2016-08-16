53d592b1d980667630ff43eb6dec3a0b0cd544a6
willdunlop <william.dunlop.1993@gmail.com>
2016-08-16 13:40:52 +1000

    16/08 -v1

--- Ruby-Mine.rb
+++ Ruby-Mine.rb
@@ -2,8 +2,7 @@
 ##### WEEEK 2 CHALLENGE #####
   ###     RUBY MINER    ###
    #######################
-require 'elscripto'
-
+require 'artii'
 
 
 
@@ -15,13 +14,30 @@ class RubyMiner
 
 
   def intro
-    puts ""
+    system "clear"
+    @con = 0
+    @zz = Artii::Base.new :font => 'slant'
+    until @con == 20 do
+      system "clear"
+      puts "\e[31m#{@zz.asciify('RUBY-MINER!')}\e[0m"
+      puts "\e[33m\t\tSeizure Edition\e[0m"
+      sleep(0.05)
+      system "clear"
+      puts "\e[33m#{@zz.asciify('RUBY-MINER!')}\e[0m"
+      puts "\e[31m\t\tSeizure Edition\e[0m"
+      sleep(0.05)
+      @con = @con + 1
+
+    end
+    main
   end
+
+
   def main          #Main menu
     efunc.header
     efunc.tcheck
     puts"\n\e[33m[1]\e[31m\tExercises\n\e[33m[2]\e[31m\tCreate File\n\e[33m[3]\e[31m\tKoans"
-    puts "\e[33m[4]\e[31m\tAbout\n\e[33m[5]\e[31m\tExit\e[0m"
+    puts "\e[33m[4]\e[31m\tGit Push\n\e[33m[5]\e[31m\tAbout\n\e[33m[6]\e[31m\tExit\e[0m"
     efunc.footer
     print "\e[37mENTER NUMBER: "
     @m = gets.chomp
@@ -39,8 +55,11 @@ class RubyMiner
       sleep(4)
       main
     when "4"
-      about
+      gitpush
+
     when "5"
+      about
+    when "6"
       efunc.header
       puts "\e[37mThank you for trying Ruby Miner"
       puts "\n\n\e[37mAll rights reserved © Ruby Miner 2016\nBy William Dunlop\e[0m"
@@ -53,66 +72,79 @@ class RubyMiner
 
   def exercise       #Exercises menu
     efunc.header
-    puts "      \e[37m     Exercises\e[0m"
+    puts "\t\e[37m  Exercises\e[0m"
+    puts ""
     exdir = '/home/will/CoderFactory/LinuxCFWork'
     filelist =  Dir.foreach(exdir).select { |x| File.file?("#{exdir}/#{x}") }
       filelist.map.each_with_index do |fl,i|
         puts "\e[33m[#{i+1}]\e[31m\t#{fl}\e[0m"
       end
+      puts "\n\e[33m[0]\t\e[36mReturn to Menu\e[0m"
     @in = filelist.each_index
     @in
     efunc.footer
     print "\e[37mENTER NUMBER: "
     @r = gets.chomp.to_i
     puts "\e[0m"
-    efunc.header
-    puts "\n\t\e[33m#{@r}. \e[31m#{filelist[@r-1]}\e[0m\n"
-    puts "\n\e[37mWhat would you like to do with this file?\e[0m"
-    puts "\e[33m[1]\e[0m \e[31mView Code \e[37m| \e[33m[2] \e[31mEdit Code\n\e[33m[3]\e[31m Run Code \e[37m | \e[33m[Any]\e[31m Return\e[37m"
-    efunc.footer
-    print "\e[37mENTER NUMBER: "
-    whatdo = gets.chomp
-    puts "\e[0m"
-    case whatdo
-    when "1"            #view code
+    if @r == 0
+      main
+    elsif @r > filelist.count
       efunc.header
-      system "cat #{filelist[@r-1]}"
-      puts "\n\e[37mPress any key to quit back to menu\e[0m"
-      qui = gets.chomp
-      if qui == "q"
+      puts "\n\e[31mIncorrect input, please try again\e[0m"
+      efunc.footer
+      sleep(2)
+      exercise
+    else
+      efunc.header
+      puts "\n\t\e[33m#{@r}. \e[31m#{filelist[@r-1]}\e[0m\n"
+      puts "\n\e[37mWhat would you like to do with this file?\e[0m"
+      puts "\e[33m[1]\e[0m \e[31mView Code \e[37m| \e[33m[2] \e[31mEdit Code\n\e[33m[3]\e[31m Run Code \e[37m | \e[33m[Any]\e[31m Return\e[37m"
+      efunc.footer
+      print "\e[37mENTER NUMBER: "
+      whatdo = gets.chomp
+      puts "\e[0m"
+      case whatdo
+      when "1"            #view code
+        efunc.header
+        system "cat #{filelist[@r-1]}"
+        puts "\n\e[37mPress any key to quit back to menu\e[0m"
+        qui = gets.chomp
+        if qui == "q"
+          exercise
+        else
+          exercise
+        end
+      when "2"          #Edit code
+        efunc.header
+        puts "Launching Atom\n..."
+        system "sudo atom #{filelist[@r-1]}"
+        sleep(1)
+        puts "Returning to menu"
+        sleep(2)
+        exercise
+      when "3"          #Run code
+        efunc.header
+        puts "Launching Application\n..."
+        system "mate-terminal --window --title='Ruby-Miner v1.0 - #{filelist[@r-1]}' -e 'ruby #{filelist[@r-1]}'"
+        sleep(1)
+        puts "Returning to menu"
+        sleep(2)
         exercise
       else
         exercise
       end
-    when "2"          #Edit code
-      efunc.header
-      puts "Launching Atom\n..."
-      system "sudo atom #{filelist[@r-1]}"
-      sleep(1)
-      puts "Returning to menu"
-      sleep(2)
-      exercise
-    when "3"          #Run code
-      efunc.header
-      puts "Launching Application\n..."
-      system "mate-terminal --window --title='Ruby-Miner v1.0 - #{filelist[@r-1]}' -e 'ruby #{filelist[@r-1]}'"
-      sleep(1)
-      puts "Returning to menu"
-      sleep(2)
-      exercise
-    else
-      exercise
     end
   end
 
   def makfile
     efunc.header
-    puts "\t\e[37mFile Creater"
-    puts "\nFile creater is a tool in Ruby Miner
-that allows you to create and set up '.rb'
-files in you 'Exercises' directory. It will
-ask you whether or not you want to set up your
-classes or attributes now or later."
+    puts "\t\e[37m  File Creater"
+    puts "\n\e[37mFile creater is a tool in Ruby Miner
+that allows you to create and set up
+'.rb'files in you 'Exercises' directory.
+It willask you whether or not you want to
+set up your classes or attributes now or
+later."
     puts "\n \e[31mWhat is the name of your project?\e[0m"
     efunc.footer
     print "\e[37mPROJECT NAME: "
@@ -200,6 +232,23 @@ classes or attributes now or later."
     end
   end
 
+  def gitpush
+    efunc.header
+    print "Enter a commit comment: "
+    comit = gets.chomp
+    puts "Launching seperate terminal for git"
+    system "mate-terminal --window --title='Ruby-Miner v1.0 - GitPusher' -e 'cd /home/will/CoderFactory/LinuxCFWork && git init && git add . && git commit -m '#{comit}' && git push -u origin master'"
+    puts "..."
+    sleep(2)
+    puts "git push was successful"
+    efunc.footer
+    sleep(1)
+    puts "Returning to menu"
+    sleep(1)
+    main
+
+  end
+
   def koans
 
   end
@@ -236,7 +285,7 @@ end
 #################################
 #################################
 
-class ExtraFunc < RubyMiner
+class ExtraFunc
   def initialize()
   end
 
@@ -272,4 +321,4 @@ end
 
 #end
 launch = RubyMiner.new(ExtraFunc.new())
-launch.main
+launch.intro
