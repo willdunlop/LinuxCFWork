4c3190d9d4e2de422bfd2bbfc7a12e7b5a03557c
willdunlop <william.dunlop.1993@gmail.com>
2016-08-21 21:26:21 +1000

    21/08 -v1

---
+++ freetime/Rhymer/rhymer.rb
@@ -0,0 +1,33 @@
+
+def rhymes
+  ay = ["clay", "cray", "day", "fray", "frais", "fray", "frey", "gai", "gay", "gaye", "gray", "graye", "grey", "guay", "haigh", "gay", "grey", "hay", "hey", "j", "jay", "lay", "they", "may", "nay", "neigh", "pay", "play", "pray", "prey", "ray", "say", "slay", "sleigh", "splay", "spray", "stay", "stray", "tre", "way", "weigh", "yay"]
+
+  # ee =
+  # ie =
+  # oh =
+  # yu =
+  #
+  # a =
+  # e =
+  # i =
+  # o =
+  # u =
+
+  ayone = ay.sample
+  aytwo = ay.sample
+  aythree = ay.sample
+  ayfour = ay.sample
+  ayfive = ay.sample
+  aysix = ay.sample
+
+
+  puts "------------------------------------"
+  puts "     | #{ayone} | #{aytwo} | #{aythree} |      "
+  puts "------------------------------------"
+  puts "     | #{ayfour} | #{ayfive} | #{aysix} |      "
+  puts "------------------------------------"
+
+
+end
+
+rhymes
